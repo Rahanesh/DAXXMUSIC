40c5627ec131b6c8d18b6a3add7696cd233420b7
Rahanesh <88672205+Rahanesh@users.noreply.github.com>
2024-03-31 20:10:32 +0330

    Update buttons.py

--- DAXXMUSIC/utils/stuffs/buttons.py
+++ DAXXMUSIC/utils/stuffs/buttons.py
@@ -1,18 +1,18 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram import Client, filters, enums 
+from pyrogram import Client, filters, enums
 
 class BUTTONS(object):
-    MBUTTON = [[InlineKeyboardButton("CʜᴀᴛGPT", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("ɢʀᴏᴜᴘs", callback_data="mplus HELP_Group"),InlineKeyboardButton("sᴛɪᴄᴋᴇʀs", callback_data="mplus HELP_Sticker")],
-    [InlineKeyboardButton("Tᴀɢ-Aʟʟ", callback_data="mplus HELP_TagAll"),
-    InlineKeyboardButton("Iɴꜰᴏ", callback_data="mplus HELP_Info"),InlineKeyboardButton("Exᴛʀᴀ", callback_data="mplus HELP_Extra")],
-    [InlineKeyboardButton("Iᴍᴀɢᴇ", callback_data="mplus HELP_Image"),
-    InlineKeyboardButton("Aᴄᴛɪᴏɴ", callback_data="mplus HELP_Action"),InlineKeyboardButton("Sᴇᴀʀᴄʜ", callback_data="mplus HELP_Search")],    
-    [InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_Font"),
-    InlineKeyboardButton("ɢᴀᴍᴇs", callback_data="mplus HELP_Game"),InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG")],
-    [InlineKeyboardButton("ɪᴍᴘᴏsᴛᴇʀ", callback_data="mplus HELP_Imposter"),
-    InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("ʜᴀsᴛᴀɢ", callback_data="mplus HELP_HT")], 
-    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
-    InlineKeyboardButton("ғᴜɴ", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
+    MBUTTON = [[InlineKeyboardButton("چت جی‌پی‌تی💬", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("گروه‌ها🧩", callback_data="mplus HELP_Group"),InlineKeyboardButton("استیکرها🥁", callback_data="mplus HELP_Sticker")],
+    [InlineKeyboardButton("برچسب‌گذاری همه🔅", callback_data="mplus HELP_TagAll"),
+    InlineKeyboardButton("اطلاعات❗️", callback_data="mplus HELP_Info"),InlineKeyboardButton("اضافی", callback_data="mplus HELP_Extra")],
+    [InlineKeyboardButton("تصویر🖼", callback_data="mplus HELP_Image"),
+    InlineKeyboardButton("عملیات⚜️", callback_data="mplus HELP_Action"),InlineKeyboardButton("جستجو🔍", callback_data="mplus HELP_Search")],    
+    [InlineKeyboardButton("فونت🖊", callback_data="mplus HELP_Font"),
+    InlineKeyboardButton("بازی‌ها🎮", callback_data="mplus HELP_Game"),InlineKeyboardButton("تلگراف🛜", callback_data="mplus HELP_TG")],
+    [InlineKeyboardButton("تقلب🤹‍♀️", callback_data="mplus HELP_Imposter"),
+    InlineKeyboardButton("حقیقت یا جرات🎭", callback_data="mplus HELP_TD"),InlineKeyboardButton("هشتگ🎵", callback_data="mplus HELP_HT")], 
+    [InlineKeyboardButton("متن به صدا🔊", callback_data="mplus HELP_TTS"),
+    InlineKeyboardButton("سرگرمی🏓", callback_data="mplus HELP_Fun"),InlineKeyboardButton("نقل قول🪧", callback_data="mplus HELP_Q")],          
     [InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
     InlineKeyboardButton(">", callback_data=f"managebot123 settings_back_helper"),
     ]]
