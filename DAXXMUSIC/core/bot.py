581f8e83a10be4d156f2e57ac317a6c7beb337e4
Rahanesh <88672205+Rahanesh@users.noreply.github.com>
2024-04-01 15:53:33 +0330

    Update bot.py

--- DAXXMUSIC/core/bot.py
+++ DAXXMUSIC/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class DAXX(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"شروع ربات...")
         super().__init__(
             name="DAXXMUSIC",
             api_id=config.API_ID,
@@ -28,7 +28,7 @@ class DAXX(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\نام : {self.name}\نام کاربری : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
