2e636c13bb5d53d157395c4ad16d69c5ce449442
Rahanesh <88672205+Rahanesh@users.noreply.github.com>
2024-04-01 04:21:03 +0330

    Update mene.py
    میم ها

--- DAXXMUSIC/plugins/Yumi/mene.py
+++ DAXXMUSIC/plugins/Yumi/mene.py
@@ -2,30 +2,30 @@ from pyrogram import Client, filters
 import requests
 from DAXXMUSIC import app 
 
-# Define a command handler for the /meme command
-@app.on_message(filters.command("meme"))
+# تعریف یک دستور برای دستور /میم
+@app.on_message(filters.command(["میم", "meme"]))
 def meme_command(client, message):
-    # API endpoint for random memes
+    # آدرس API برای میم‌های تصادفی
     api_url = "https://meme-api.com/gimme"
 
     try:
-        # Make a request to the API
+        # ارسال درخواست به API
         response = requests.get(api_url)
         data = response.json()
 
-        # Extract the meme image URL
+        # استخراج آدرس تصویر میم
         meme_url = data.get("url")
         title = data.get("title")
 
-        # Mention the bot username in the caption
-        caption = f"{title}\n\nRequest by {message.from_user.mention}\nBot username: @{app.get_me().username}"
+        # افزودن نام کاربری ربات در زیرنویس
+        caption = f"{title}\n\nدرخواست شده توسط {message.from_user.mention}\nنام کاربری ربات: @{app.get_me().username}"
 
-        # Send the meme image to the user with the modified caption
+        # ارسال تصویر میم به کاربر با زیرنویس تغییر یافته
         message.reply_photo(
             photo=meme_url,
             caption=caption
         )
 
     except Exception as e:
-        print(f"Error fetching meme: {e}")
-        message.reply_text("Sorry, I couldn't fetch a meme at the moment.")
+        print(f"خطا در دریافت میم: {e}")
+        message.reply_text("با عرض پوزش، در حال حاضر نمی‌توان میمی را دریافت کرد.")
